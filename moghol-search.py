5896d0e3579392471eae9e82751d0fa0183b20d3
ajpk <41717689+anthonyjk@users.noreply.github.com>
2023-09-18 20:09:55 -0400

    Declination Table prototype
    TODO: New dictonary with each word having attributes (noun/verb/adj/etc).
    Also: Declination table needs more accuracy

--- moghol-search.py
+++ moghol-search.py
@@ -5,8 +5,14 @@ root.geometry("145x145")
 root.wm_title("Dictonary Search")
 search = tk.StringVar()
 
+#expanded_table = tk.Tk()
+
+# Searches Moghol Dictonary for Word
 def dict_search():
     global search
+    
+    #expanded_table.destroy()
+    
     word = search.get()
     with open("dictonary.txt") as f:
         moheng = f.read().splitlines()
@@ -17,11 +23,42 @@ def dict_search():
             return
     display("No translation found.")
 
+# Table controller and translation display
 def display(word):
     translation_label.config(text = word)
+    declination_table(word)
+
+class Table:
+
+    def __init__(self,root,grmr_names,modifiers,word):
+        for i in range(2):
+            for j in range(len(grmr_names)):
+                if i == 0:
+                    self.lbl = tk.Label(root)
+
+                    self.lbl.grid(row=j, column=i)
+                    self.lbl.config(text=grmr_names[j])
+
+                if i == 1:
+                    self.wrd = tk.Label(root)
+
+                    self.wrd.grid(row=j, column=i)
+                    self.wrd.configure(text=word+modifiers[j])
+                    
+# Noun declination table pop-up
+def declination_table(word):
+    declinations = ["Nomative", "Genitive-Accusative", "Dative-Locative","Ablative","Instrumental","Comitative"]
+    endings = ["", "i", "du", "asa", "ar", "lei"] # Not 100% accurate
+    expanded_table = tk.Tk()
+    expanded_table.geometry("300x300")
+    expanded_table.title("Declination Table")
+    d_t = Table(expanded_table,declinations,endings,word)
+    expanded_table.mainloop()
+            
     
     
-srh_entry = tk.Entry(root,textvariable = search, font=('calibre',10))
+    
+srh_entry = tk.Entry(root,textvariable = search, font=('arial',10))
 srh_btn=tk.Button(root, text="Search", command = dict_search)
 translation_label = tk.Label(root, text="")
 
